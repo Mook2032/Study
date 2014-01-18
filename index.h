21ba99f800b893b093a6185cd09e235d1a4a49e0
Mook2032 <curseofwallachia@gmail.com>
2014-01-18 12:53:35 +0700

    Update index.h

--- index.h
+++ index.h
@@ -1,5 +1,10 @@
+//Week 1's homework: Index
+
 #define UPDATE 1
 #define INSERT 0
+
+//MAX_LINE is the maximum number of character can be stored in wrdoccurence.
+//MAX_WORD is the maximum number of different words.
 
 #define MAX_LINE 1000
 #define MAX_WORD 2000
@@ -28,10 +33,10 @@ typedef struct _indexes_state
 void decaption(char* str);
 int NewIndexes(IndexesState *state);
 int unneeded_word_load(FILE *f);
-int index_search(String newword,IndexesState *state,int *position);
+int index_search(String newword,IndexesState *state,int *position);	//Binary search, and if not found, return the position for insertion sort
 inline void indexes_copy(IndexesState *state,int from,int to);
 void pusher(IndexesState *state,int position);
-int insert_wordlist(String newword,IndexesState *state,int position,int linenum);
+int insert_wordlist(String newword,IndexesState *state,int position,int linenum);	//Using insertion sort
 void update_wordlist(String newword,IndexesState *state,int position,int linenum);
 void upsert_wordlist(String newword,IndexesState *state,int linenum);
 inline void fgetchar_count_line(FILE *f,char *ch,int *linenum);
