605eac80a9a828f8ac96b605cd3b308c1a76ac5b
Mook2032 <curseofwallachia@gmail.com>
2014-02-18 18:17:53 +0700

    Some fix to include Float and String

--- week2/scanner.c
+++ week2/scanner.c
@@ -79,9 +79,36 @@ Token* readIdentKeyword(void) {
 	
 }
 
-//Copy paste from above
+//Dear me, we need a special function to work with float number first.
+//We will have to call it from inside readNumber(), so it will be the only odd ball in "read series"
+//	that take a Token as argument.
+Token* readFloat(Token* token,int count) {
+	token->string[count++]='.';
+	token->tokenType=TK_FLOAT;
+	readChar();
+	//copy paste again, from readNumber below
+	while (charCodes[currentChar]==CHAR_DIGIT)
+	{
+		if (count<MAX_IDENT_LEN)
+		{
+			token->string[count++]=(char)currentChar;
+			readChar();											//Dear me, the finisher is here.
+			if (currentChar==EOF) break;
+		}
+		else
+		{
+			error(ERR_IDENTTOOLONG,token->lineNo,token->colNo);
+			token->string[MAX_IDENT_LEN]='\0';
+		}
+	}
+	token->string[count]='\0';
+	token->valueFloat=atof(token->string);
+	
+	return token;
+}
+
 Token* readNumber(void) {
-	Token* token=makeToken(TK_NUMBER,lineNo,colNo);
+	Token* token=makeToken(TK_INT,lineNo,colNo);
 	int count=0;
 	while (charCodes[currentChar]==CHAR_DIGIT)
 	{
@@ -97,6 +124,7 @@ Token* readNumber(void) {
 			token->string[MAX_IDENT_LEN]='\0';
 		}
 	}
+	if (charCodes[currentChar]==CHAR_PERIOD) return readFloat(token,count);	//It's over yet!!
 	
 	token->string[count]='\0';
 	token->value=atoi(token->string);
@@ -108,9 +136,9 @@ Token* readConstChar(void) {
 	Token *token = makeToken(TK_CHAR, lineNo, colNo);
 	readChar();
 	if (currentChar == EOF) {
-		error(ERR_INVALIDCHARCONSTANT, token->lineNo, token->colNo);
 		token->tokenType = TK_NONE;
-		return token;
+		error(ERR_INVALIDCHARCONSTANT, token->lineNo, token->colNo);	//The program should quit here, but...
+		return token;		//...just to be safe.
 	}
 	token->string[0] = currentChar;
 	token->string[1] = '\0';
@@ -129,6 +157,40 @@ Token* readConstChar(void) {
 	return token;
 }
 
+Token* readString(void) {
+	Token* token=makeToken(TK_STRING, lineNo, colNo);
+	int count=0;
+	readChar();
+	if (currentChar == EOF) {
+		token->tokenType = TK_NONE;
+		error(ERR_INVALIDSTRING, token->lineNo, token->colNo);	//The program should quit here, but...
+		return token;		//...just to be safe.
+	}	//Copy from above. And let's add a new error message, as well.
+	while (charCodes[currentChar]!=CHAR_QUOTE)
+	{
+		if (count<MAX_IDENT_LEN)
+		{
+			token->string[count++]=(char)currentChar;
+			readChar();
+			if (currentChar==EOF)
+			{
+				token->tokenType = TK_NONE;
+				error(ERR_INVALIDSTRING, token->lineNo, token->colNo);
+				return token;
+			}
+		}
+		else
+		{
+			error(ERR_IDENTTOOLONG,token->lineNo,token->colNo);
+			return token;
+		}
+	}
+	token->string[count]='\0';
+	readChar();				//Uh, unlike other function, this one stop when it get to see that CHAR_QUOTE dude,
+							//so we have to append one to the end.
+	return token;
+}
+
 //Dear me, it seems that you have to call readChar() at the end of every case, or else things won't go as we want.
 //You should probably make the function use readChar() as a staple finisher, too.
 Token* getToken(void) {
@@ -143,6 +205,7 @@ Token* getToken(void) {
   case CHAR_LETTER: return readIdentKeyword();
   case CHAR_DIGIT: return readNumber();
   case CHAR_SINGLEQUOTE: return readConstChar();
+  case CHAR_QUOTE: return readString();
   case CHAR_PLUS: 
     token = makeToken(SB_PLUS, lineNo, colNo);
     readChar(); 
@@ -291,9 +354,11 @@ void printToken(Token *token) {
   switch (token->tokenType) {
   case TK_NONE: printf("TK_NONE\n"); break;
   case TK_IDENT: printf("TK_IDENT(%s)\n", token->string); break;
-  case TK_NUMBER: printf("TK_NUMBER(%s)\n", token->string); break;
+  case TK_INT: printf("TK_INT(%s)\n", token->string); break;
   case TK_CHAR: printf("TK_CHAR(\'%s\')\n", token->string); break;
   case TK_EOF: printf("TK_EOF\n"); break;
+  case TK_FLOAT: printf("TK_FLOAT(%s)\n", token->string); break;
+  case TK_STRING: printf("TK_STRING(%s)\n", token->string); break;
 
   case KW_PROGRAM: printf("KW_PROGRAM\n"); break;
   case KW_CONST: printf("KW_CONST\n"); break;
